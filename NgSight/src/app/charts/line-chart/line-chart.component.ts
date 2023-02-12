c6226af8e4e697e06013d4f6aee07f980b7ba191
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2023-02-12 14:31:04 +0100

    Added line chart with sample data

--- NgSight/src/app/charts/line-chart/line-chart.component.ts
+++ NgSight/src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType, ChartConfiguration } from 'chart.js';
+import { LINE_CHART_SAMPLE_DATA } from '../../shared/chart.colors';
+
+
+const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'March', 'Apr', 'May'];
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +13,20 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  lineChartData: ChartConfiguration['data'] = {
+    datasets: LINE_CHART_SAMPLE_DATA,
+    labels: LINE_CHART_LABELS
+  };
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions:ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {display: true}
+    }
+  };
+  // lineChartLegend: true;
+  lineChartType:ChartType = 'line';
 
   ngOnInit(): void {
   }
--- NgSight/src/app/charts/pie-chart/pie-chart.component.ts
+++ NgSight/src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-pie-chart',
@@ -8,6 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class PieChartComponent implements OnInit {
 
   constructor() { }
+
+  public pieChartData: ChartData<'pie', number[], string|string[]> ={
+    labels: ['Mike', 'Joe', 'Mat'],
+    datasets:[
+      {
+        data:[350,450,120],
+        backgroundColor: ['#26547c', '#ff6b6b', '$ffd166'],
+        borderColor: ['#26547c', '#ff6b6b', '$ffd166']
+      }
+    ],
+  } 
+  pieChartType:ChartType = 'pie';
 
   ngOnInit(): void {
   }
