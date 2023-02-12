3c9c786548bbe10f9fc04cd345f2173be2e5eb87
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2023-02-12 19:16:45 +0100

    Updated server component

--- NgSight/src/app/server/server.component.ts
+++ NgSight/src/app/server/server.component.ts
@@ -13,11 +13,34 @@ export class ServerComponent implements OnInit {
     // Ref: https://bobbyhadz.com/blog/typescript-interface-empty-object 
     // and https://stackoverflow.com/questions/49699067/property-has-no-initializer-and-is-not-definitely-assigned-in-the-construc
     this.serverInput = {} as Server;
+    this.color = '';
+    this.buttonText = '';
   }
 
+  color: string;
+  buttonText: string;
   @Input() serverInput: Server;
 
   ngOnInit(): void {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66BB6A',
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    console.log(this.serverInput.name, ': ', onlineStatus);
+    this.setServerStatus(!onlineStatus);
   }
 
 }
