b1090c5b50a2d959b47e51e48ccab4e90e224dd1
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2023-02-12 18:06:32 +0100

    Latest Orders page

--- NgSight/src/app/sections/section-orders/section-orders.component.ts
+++ NgSight/src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order} from "../../shared/order";
 
 @Component({
   selector: 'app-section-orders',
@@ -9,7 +10,31 @@ export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  orders: Order[] = [
+    { id: 1, 
+      customer: {
+        id: 1, 
+        name: "Joe", 
+        state:"CO", 
+        email:"joe@testing.com"
+      },
+      total: 230,
+      placed: new Date(2023,2,1),
+      fulfilled: new Date(2023,2,5)
+    },
+    { id: 2, 
+      customer: {
+        id: 1, 
+        name: "Matt", 
+        state:"TX", 
+        email:"Matt@testing.com"
+      },
+      total: 453,
+      placed: new Date(2023,3,1),
+      fulfilled: new Date(2023,3,5)
+    },
+    ];
+
+  ngOnInit(): void {}
 
 }
